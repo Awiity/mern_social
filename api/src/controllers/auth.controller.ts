9260130557da0d2ee2cf0092b26dfe7875cab501
Ruslan Kildibekov <awitkild28.08@gmail.com>
2025-07-11 18:20:23 +0200

    fix: update cookie domain to include subdomain for production environment

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -29,14 +29,14 @@ export const AuthController = {
                 secure: process.env.NODE_ENV == "production" ? true : true,
                 maxAge: 15 * 60 * 1000,                         // 15min
                 sameSite: process.env.NODE_ENV == "production" ? 'none' : 'lax',
-                domain: process.env.NODE_ENV === "production" ? "opal-social-mocha.vercel.app" : undefined
+                domain: process.env.NODE_ENV === "production" ? ".opal-social-mocha.vercel.app" : undefined
             });
             res.cookie("refreshToken", refreshToken, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV == "production" ? true : true,
                 maxAge: 1 * 24 * 60 * 60 * 1000,                         // 24 hours / 1 day
                 sameSite: process.env.NODE_ENV == "production" ? 'none' : 'lax',
-                domain: process.env.NODE_ENV === "production" ? "opal-social-mocha.vercel.app" : undefined
+                domain: process.env.NODE_ENV === "production" ? ".opal-social-mocha.vercel.app" : undefined
 
             });
 
