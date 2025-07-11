a3082489f123ce27cc678c90b3fddaca949aa374
Ruslan Kildibekov <awitkild28.08@gmail.com>
2025-07-11 16:42:39 +0200

    Testing cors

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -6,7 +6,7 @@ const mongodb_uri = process.env.MONGODB_URI || config.mongodb_uri;
 export const connectDB = async () => {
     try {
         mongoose.connect(mongodb_uri);
-        console.log("Successfuly connected to MongoDB");
+        console.log("Successfuly connected to MongoDB", config.client_url);
     } catch (e) {
         console.log("MongoDB connection failed, Error: ", e);
         process.exit(1);
