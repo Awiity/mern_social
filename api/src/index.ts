323568f5b1cb1e319e192705510fc51db48d4881
Ruslan Kildibekov <awitkild28.08@gmail.com>
2025-07-11 14:24:59 +0200

    feat: add express-rate-limit middleware to limit request rates and enhance API security

--- api/src/index.ts
+++ api/src/index.ts
@@ -22,11 +22,21 @@ import { Types } from 'mongoose';
 import messageRoutes from './routes/message.routes';
 import likeRoutes from './routes/like.routes';
 import commentRoutes from './routes/comment.routes';
+import rateLimit from 'express-rate-limit';
 
 const cors = require('cors');
 const http = require('http');
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Too many requests from this IP, please try again later',
+});
+
 const server = http.createServer(app);
 const io = new Server<
     ClientToServerEvents,
@@ -259,7 +269,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 app.use(express.json());
-
+app.use(limiter);
 app.use(cookieParser());
 connectDB();
 
